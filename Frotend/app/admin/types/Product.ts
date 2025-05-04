f987ad91dbd7026edc1106cfb3c198c412f37b5f
Vlad <chvs1312@gmail.com>
2025-05-04 22:55:47 +0300

    fix: remove reserve copy

--- Frotend/app/admin/types/Product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    name: string;
-    description: string;
-    price: number;
-    stock: number;
-    imageUrl?: string;
-    categoryId: string;
-  }
-  
--- Frotend/app/metadata.ts
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-    title: "STROYZONE",
-    description: "Описание проекта",
-  };
-  
